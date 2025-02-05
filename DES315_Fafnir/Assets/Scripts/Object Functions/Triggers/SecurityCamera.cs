8bccbf4328ffb5506c21473756b3a776dcf8db03
2304542 <2304542@uad.ac.uk>
2025-02-05 16:30:56 +0000

    Adding script for security camera
    Trigger testing at this point

---
+++ DES315_Fafnir/Assets/Scripts/Object Functions/Triggers/SecurityCamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    [SerializeReference] private GameObject security;
+    public Material closedMaterial;
+    public Material openMaterial;
+    private bool cameraOn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (cameraOn)
+        {
+            Debug.Log("You're'nt winner!");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+    }
+}
