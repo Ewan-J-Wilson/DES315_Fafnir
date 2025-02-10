5d9d80df397499b9cb69b2a672a89d52a2661ac2
Lily McEwan <2202567@uad.ac.uk>
2025-02-10 15:01:22 +0000

    Audio volume can be saved

--- DES315_Fafnir/Assets/Scripts/Button Functions/VolumeSliders.cs
+++ DES315_Fafnir/Assets/Scripts/Button Functions/VolumeSliders.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +6,21 @@ public class VolumeSliders : MonoBehaviour
     [SerializeField] AudioType AudioBus;
 
     public void Start()  
-    { GetComponent<Slider>().onValueChanged.AddListener(ValueChanged); }
+    { 
+        Slider slider = GetComponent<Slider>();
+        // Load the audio level from settings
+        slider.value = PlayerPrefs.GetFloat("Audio: " + AudioBus.ToString());
+        // Add the trigger for the slider being changed
+        slider.onValueChanged.AddListener(ValueChanged);
+    }
 
     private void ValueChanged(float value)
-    { Audiomanager.ChangeVolume(value, AudioBus); }
+    { 
+        // Change the volume
+        Audiomanager.ChangeVolume(value, AudioBus);
+        // Save the new value
+        PlayerPrefs.SetFloat("Audio: " + AudioBus.ToString(), value);
+        PlayerPrefs.Save();
+    }
 
 }
