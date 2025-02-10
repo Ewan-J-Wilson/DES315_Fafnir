9fafeab7310c6fd104e834c05d8eb473785a5abc
2304542 <2304542@uad.ac.uk>
2025-02-10 17:48:05 +0000

    Commenting

--- DES315_Fafnir/Assets/Scripts/Object Functions/Triggers/SecurityCamera.cs
+++ DES315_Fafnir/Assets/Scripts/Object Functions/Triggers/SecurityCamera.cs
@@ -6,24 +6,22 @@ using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
-    [SerializeReference] private GameObject security;
+    [SerializeReference] private GameObject security; 
     [SerializeReference] PlayerAI player;
-    public Material closedMaterial;
-    public Material openMaterial;
-    private bool cameraOn = true;
+    private bool cameraOn = true; // keep this boolean 
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) // destroys clones when player enters camera trigger
     {
-        if (collision.tag != "Player")
+        if (collision.tag != "Player") // ensures that this only occurs when the player is inside the trigger, and not the clones themselves
         {
             return;
         }
-        player.KillClone();
+        player.KillClone(); // calls function from player script 
     }
     // Update is called once per frame
     void Update()
