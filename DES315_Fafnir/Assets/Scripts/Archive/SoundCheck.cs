1233aa9d37a67e629a99a117bf35d05557f4b8b0
Lily McEwan <2202567@uad.ac.uk>
2025-02-17 15:36:57 +0000

    Removed redundant menu buttons
    - also cleaned up some code for future refactoring

--- DES315_Fafnir/Assets/Scripts/Archive/SoundCheck.cs
+++ DES315_Fafnir/Assets/Scripts/Archive/SoundCheck.cs
@@ -5,21 +5,10 @@ using UnityEngine;
 public class SoundCheck : MonoBehaviour
 {
 
-    void Awake() {
-
-        
-
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         Audiomanager.instance.PlayAudio("Minceraft");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
