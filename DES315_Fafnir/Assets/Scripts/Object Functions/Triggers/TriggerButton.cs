7de86c2a7df66cfb4d1a042e1a3ba7ba476f4204
Lily McEwan <2202567@uad.ac.uk>
2025-03-18 16:25:33 +0000

    Cleanup functions pt 1

--- DES315_Fafnir/Assets/Scripts/Object Functions/Triggers/TriggerButton.cs
+++ DES315_Fafnir/Assets/Scripts/Object Functions/Triggers/TriggerButton.cs
@@ -2,12 +2,7 @@ using UnityEngine;
 
 public class TriggerButton : TriggerGeneric
 {
-    //private const float PressSpeed = 2f;//Speed at which the button is pressed down
-    //private Transform ButtonTrans;
-    //private Vector3 ButtonPos;          //Original position of button
-    //private Vector3 PressedPos;         //Position for button to go to when fully pressed
-    //private float ButtonTimer;          //Time to wait for button to press down
-    
+
     [SerializeField]
     [Tooltip("TEMP: Colour for when the button is off")]
     private Color offColour;
@@ -17,23 +12,11 @@ public class TriggerButton : TriggerGeneric
 
     private bool trigger = false;
 
-    //private void Start()
-    //{
-    //    //ButtonTrans = GetComponentInChildren<Transform>();
-    //    //ButtonPos = transform.position;
-    //    //PressedPos = ButtonPos + new Vector3(0, -0.5f, 0);
-    //}
 
     private void Update()
     {
 
         GetComponent<SpriteRenderer>().color = isActive ? onColour : offColour;
-
-        //// Updates the button timer 
-        //if (isActive && ButtonTimer != 1f)
-        //{ ButtonTimer = Mathf.Clamp(ButtonTimer += Time.deltaTime * PressSpeed, 0, 1); }
-        //else if (!isActive && ButtonTimer != 0f)
-        //{ ButtonTimer = Mathf.Clamp(ButtonTimer -= Time.deltaTime * PressSpeed, 0, 1); }
 
         // The frame the button is pressed down
         if (!trigger && isActive) { 
@@ -47,10 +30,6 @@ public class TriggerButton : TriggerGeneric
         }
 
     }
-
-    // Animates the button being pressed
-    //private void FixedUpdate()
-    //{ ButtonTrans.position = Vector3.MoveTowards(ButtonPos, PressedPos, ButtonTimer); }
 
     // Detects the player on the button
     private void OnTriggerStay2D(Collider2D collision) { 
