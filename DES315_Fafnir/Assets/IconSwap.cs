ef1e7eba6a7c4208fbcb8ebac2773a96da974a1f
Lily McEwan <2202567@uad.ac.uk>
2025-03-21 11:16:28 +0000

    Next Dialogue Prompt Blinking Added

--- DES315_Fafnir/Assets/IconSwap.cs
+++ DES315_Fafnir/Assets/IconSwap.cs
@@ -23,19 +23,53 @@ public class IconSwap : MonoBehaviour
     private Sprite keyboard;
 
     private PlayerInput input;
+    private string deviceName = "";
+    private string controlSchemeName = "";
+
+    private float blinkTimer = 0;
+
+    [SerializeField]
+    private float blinkSpeed = 0.75f;
+    // Blink State
+    private bool hasBlinked;
+
+    [SerializeField]
+    private bool canBlink = true;
 
     public void Start() {
 
         SceneManager.sceneLoaded += DetectInputScheme;
         DetectInputScheme(gameObject.scene, LoadSceneMode.Single);
 
-    }
-    
+        ChangeIcon();
 
-    // Update is called once per frame
-    void Update()
-    {
+    }
+
+    public void Update() {
+
+        ChangeIcon();
         
+        if (!canBlink)
+        { return; }
+
+        // Blink
+        if ((blinkTimer += Time.deltaTime) >= blinkSpeed) {
+
+            blinkTimer = 0;
+            GetComponent<Image>().color = (hasBlinked = !hasBlinked) ? Color.clear : Color.white;
+           
+        }
+
+    }
+
+    private void ChangeIcon() {
+        
+        if (input.currentControlScheme == controlSchemeName && input.devices[0].name == deviceName)
+        { return; }
+
+        controlSchemeName = input.currentControlScheme;
+        deviceName = input.devices[0].name;
+
         if (input.currentControlScheme == "Keyboard")
         { GetComponent<Image>().sprite = keyboard; }
         else {
