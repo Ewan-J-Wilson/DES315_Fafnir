baab7c6eb8fd7345015c62082672582d22907d20
Lily McEwan <2202567@uad.ac.uk>
2025-04-16 15:56:18 +0100

    Settings can be accessed from pause menu
    - Text Settings in progress

--- DES315_Fafnir/Assets/Scripts/Button Functions/Keybind Button Selector/SwapControlBinds.cs
+++ DES315_Fafnir/Assets/Scripts/Button Functions/Keybind Button Selector/SwapControlBinds.cs
@@ -1,17 +1,30 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SwapControlBinds : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject[] Panels = { };
+
     private void Awake() {
 
         //Debug.Log(GameObject.FindGameObjectWithTag("Keyboard").name);
         //Debug.Log(GameObject.FindGameObjectWithTag("Controller").name);
 
-        foreach (GameObject keyboard in GameObject.FindGameObjectsWithTag("Keyboard"))
-        { keyboard.SetActive(!MenuButtons.swapCondition); }
+    }
 
-        foreach (GameObject gamepad in GameObject.FindGameObjectsWithTag("Controller"))
-        { gamepad.SetActive(MenuButtons.swapCondition); }
+    private void OnEnable() {
+
+        foreach (GameObject obj in Panels)
+        { 
+            if (obj.CompareTag("Keyboard"))
+            { obj.SetActive(!MenuButtons.swapCondition); }
+
+            if (obj.CompareTag("Controller"))
+            { obj.SetActive(MenuButtons.swapCondition); }
+            
+        }
 
     }
 
